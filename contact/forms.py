a2415ab57e8bd4711eb98b4abe208831ed93a88c
Matheus Matos <matheeeusmatos@gmail.com>
2025-04-14 10:58:28 -0300

    colocando mensagens, login e logout

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from contact.models import Contact
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
@@ -72,3 +74,36 @@ class ContactForm(forms.ModelForm):
                 )
             )
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    email = forms.EmailField(
+        required=True,
+    )
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe esse e-mail', code='invalid')
+            )
+
+        return email
---
+++ contact/views/user_forms.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from contact.forms import RegisterForm
+from django.contrib import messages, auth
+from django.contrib.auth.forms import AuthenticationForm
+
+def register(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Usuario registrado')
+            form.save()
+            return redirect(
+                'contact:login'
+                )
+
+    return render(
+        request,
+        'contact/register.html',
+        {
+            'form': form
+        }
+
+    )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contact:index')
+        
+        messages.error(request, 'Login invalido!')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
