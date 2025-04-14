32fb2abb60f6b2101c5b5ab1baadb28f9960e7eb
Matheus Matos <matheeeusmatos@gmail.com>
2025-04-14 13:15:37 -0300

    criando relação entre o contato e o owner

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from contact.forms import ContactForm
 from contact.forms import Contact
+from django.contrib.auth.decorators import login_required
 # from django.http import Http404
 # Create your views here.
 
-
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     
@@ -38,8 +39,9 @@ def create(request):
         context,
     )
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -51,7 +53,9 @@ def update(request, contact_id):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner =  request.user
+            contact.save()
             return redirect('contact:update', contact_id= contact.pk)
 
         return render(
@@ -71,8 +75,9 @@ def update(request, contact_id):
         context,
     )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
 
     confirmation = request.POST.get('confirmation', 'no')
 
